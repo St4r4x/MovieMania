bea83a95b005bcaaeff65a12bd025d0086359c16
St4r4x <st4r4x@gmail.com>
2024-07-02 20:52:46 +0200

    chore: Update genre-based recommendation fetcher to fetch top 10 movies per genre

--- movie_recommendations_api/main.py
+++ movie_recommendations_api/main.py
@@ -1,26 +1,19 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from typing import List, Dict
+from typing import Any, Dict
 from movie_recommendations_api import models, schemas, database
 from movie_recommendations_api.recommendations import GenreBasedRecommendationFetcher, TrendingRecommendationFetcher, MovieBasedRecommendationFetcher
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=database.engine)
-@app.get("/recommendations/{user_id}", response_model=Dict[str, List[schemas.Movie]])
+@app.get("/recommendations/{user_id}", response_model=Dict[str, Any])
 async def get_recommendations(user_id: int, db: Session = Depends(database.get_db)):
-    user = db.query(models.Users).filter(models.Users.user_id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    print(f"Fetching recommendations for user {user_id}")
-    # Fetch recommendations
-    genre_fetcher = GenreBasedRecommendationFetcher()
-    trending_fetcher = TrendingRecommendationFetcher()
-    movie_based_fetcher = MovieBasedRecommendationFetcher()
-
     recommendations = {}
-    recommendations.update(genre_fetcher.fetch(db, user_id))
-    #recommendations.update(trending_fetcher.fetch(db, user_id))
-    #recommendations.update(movie_based_fetcher.fetch(db, user_id))
+    genre_fetcher = GenreBasedRecommendationFetcher()
 
+    genre_recommendations = genre_fetcher.fetch(db, user_id)
+    for key, value in genre_recommendations.items():
+        recommendations[key] = value
+    
     return recommendations
