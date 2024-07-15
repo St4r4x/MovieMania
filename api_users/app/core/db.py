8672725be4cb48f1b25a757c1e2b096038b49625
Jm GERONIMI <spawn2x@protonmail.com>
2024-07-15 18:35:09 +0200

    add database configuration + security functions + update main.py with fastAPI bases

---
+++ api_users/app/core/db.py
@@ -0,0 +1,19 @@
+from sqlmodel import Session, create_engine, select
+
+from app import crud
+from app.core.config import settings
+from app.models import User, UserCreate
+
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+def init_db(session: Session) -> None:
+    user = session.exec(
+        select(User).where(User.email == settings.FIRST_SUPERUSER)
+    ).first()
+    if not user:
+        user_in = UserCreate(
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+        user = crud.create_user(session=session, user_create=user_in)
