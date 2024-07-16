f6d7397876c6fffcabfa47ddf725b6a300b1240f
Jm GERONIMI <spawn2x@protonmail.com>
2024-07-16 20:17:10 +0200

    add env email config + fix bug with movieuser endpoints

--- users_api/app/api/routes/movieusers.py
+++ users_api/app/api/routes/movieusers.py
@@ -4,12 +4,12 @@ from fastapi import APIRouter, HTTPException
 from sqlmodel import func, select
 
 from app.api.deps import CurrentUser, SessionDep
-from app.models import MovieUser, MovieUserCreate, MovieUserOut, MovieUserOut, Message
+from app.models import MovieUser, MovieUserCreate, MovieUserOut, MovieUsersOut, MovieUserOut, Message
 
 router = APIRouter()
 
 
-@router.get("/", response_model=MovieUserOut)
+@router.get("/", response_model=MovieUsersOut)
 def read_movieusers(
     session: SessionDep, current_user: CurrentUser, skip: int = 0, limit: int = 100
 ) -> Any:
@@ -19,42 +19,28 @@ def read_movieusers(
 
     if current_user.is_superuser:
         count_statement = select(func.count()).select_from(MovieUser)
-        count = session.execute(count_statement).one()
+        count = session.execute(count_statement).scalar()
         statement = select(MovieUser).offset(skip).limit(limit)
-        movieusers_tuple = session.execute(statement).all()
+        movieusers = session.execute(statement).scalars().all()
     else:
         count_statement = (
             select(func.count())
             .select_from(MovieUser)
             .where(MovieUser.user_id == current_user.user_id)
         )
-        count = session.execute(count_statement).one()
+        count = session.execute(count_statement).scalar()
         statement = (
             select(MovieUser)
             .where(MovieUser.user_id == current_user.user_id)
             .offset(skip)
             .limit(limit)
         )
-        movieusers_tuple = session.execute(statement).all()
+        movieusers = session.execute(statement).scalars().all()
 
-    def convert_users_to_medialikesout(
-        movieusers: List[tuple],
-    ) -> List[MovieUserOut]:
-        return [
-            MovieUserOut(
-                movie_id=movieuser[0].movie_id,
-                user_id=movieuser[0].user_id,
-            )
-            for movieuser in movieusers
-        ]
-
-    movieusers_out = convert_users_to_medialikesout(movieusers_tuple)
-    print(count)
-
-    return MovieUserOut(data=movieusers_out, count=count)
+    return MovieUsersOut(data=movieusers, count=count)
 
 
-@router.get("/{id}", response_model=MovieUserCreate)
+@router.get("/{id}", response_model=MovieUserOut)
 def read_movieuser(session: SessionDep, current_user: CurrentUser, id: int) -> Any:
     """
     Get movieuser by ID.
@@ -62,7 +48,7 @@ def read_movieuser(session: SessionDep, current_user: CurrentUser, id: int) -> A
     movieuser = session.get(MovieUser, id)
     if not movieuser:
         raise HTTPException(status_code=404, detail="movieuser not found")
-    if not current_user.is_superuser and (movieuser.user_id != current_user.id):
+    if not current_user.is_superuser and (movieuser.user_id != current_user.user_id):
         raise HTTPException(status_code=400, detail="Not enough permissions")
     return movieuser
 
