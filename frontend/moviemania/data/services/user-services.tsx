27551e75818b31340d702fd0adbe34cf345faf7f
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-07-30 14:25:14 +0200

    change token for test purposes

--- frontend/moviemania/data/services/user-services.tsx
+++ frontend/moviemania/data/services/user-services.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMwMzE0MjcsInN1YiI6IjEifQ.Wo_Gh3RvmQ38TSj4bfR6DGCRXIrs6bPb3cft2bhlNZw";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMwMzI4NTQsInN1YiI6IjIifQ.BJl25Hm3ba2bo1n178zJJwGcKcWvQGHMLQoDrub465g";
 
 export const postGenresUser = async (genres: string[]) => {
 	try {
