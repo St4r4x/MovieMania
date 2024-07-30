07860102637c802c3b987e91433b9d8fa50394aa
Jm GERONIMI <spawn2x@protonmail.com>
2024-07-31 00:20:58 +0200

    add ratings & saved pages + components

--- frontend/moviemania/app/profile/saved/page.tsx
+++ frontend/moviemania/app/profile/saved/page.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-
-import styles from "./Profile.module.css";
 import profileBackground from "@/public/profile-background.png";
+import styles from "../Profile.module.css";
 import ProfileDetails from "@/src/components/profile/ProfileDetails";
 
-function Profile() {
+function MediaUserSaved() {
 	return (
 		<main className="flex flex-col items-center min-h-screen w-full gap-7">
 			<div
 				className={`${styles.background} w-full bg-no-repeat bg-cover bg-center items-end flex p-10`}
 				style={{ backgroundImage: `url(${profileBackground?.src})`, height: `690px` }}
 			>
-				<ProfileDetails enriched={true}/>
+				<ProfileDetails enriched={false}/>
 			</div>
 		</main>
 	);
 }
 
-export default Profile;
+export default MediaUserSaved;
--- frontend/moviemania/src/components/profile/ProfileDetails.tsx
+++ frontend/moviemania/src/components/profile/ProfileDetails.tsx
@@ -1,30 +1,62 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-function ProfileDetails() {
-  return (
-    <div className='flex flex-row text-white gap-14'>
-        <div className='flex flex-row items-end justify-center gap-6'>
-            <div className='rounded-full w-28 h-28 bg-purple-400'></div>
-            <div className='flex flex-col gap-2'>
-                <div className='italic'>@Profile</div>
-                <div className='text-2xl font-bold'>John</div>
-                <div className='border border-1 py-1 px-10 rounded-md border-gray-400'>
-                    <span>Edit profile</span>
-                </div>
-            </div>
-        </div>
-        <div className='flex flex-row items-end justify-center gap-5'>
-            <div className='flex flex-col items-center justify-center border-e-2 border-e-white-200 pe-5'>
-                <div className='text-xl font-bold'>5</div>
-                <div className='text-xl text-gray-400'>Ratings</div>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <div className='text-xl font-bold'>5</div>
-                <div className='text-xl text-gray-400'>Saved</div>
-            </div>
-        </div>
-    </div>
-  )
+function ProfileDetails({ enriched }: { enriched: boolean }, { page }: { page: string }) {
+	return (
+		<div className="flex flex-row text-white gap-14">
+			<div className="flex flex-row items-end justify-center gap-6">
+				{enriched ? (
+					<>
+						<div className="rounded-full w-28 h-28 bg-purple-400"></div>
+					</>
+				) : (
+					<>
+						<div className="rounded-full w-20 h-20 bg-purple-400"></div>
+					</>
+				)}
+				<div className="flex flex-col gap-2">
+					{enriched ? (
+						<>
+							<div className="italic">@Profile</div>
+							<div className="text-2xl font-bold">John</div>
+						</>
+					) : (
+						<>
+							<div className="italic">@Profile</div>
+							<div className="text-4xl font-bold">{page}</div>
+						</>
+					)}
+					{enriched ? (
+						<Link href="/settings">
+							<div className="border border-1 py-1 px-10 rounded-md border-gray-400">
+								<span>Edit profile</span>
+							</div>
+						</Link>
+					) : (
+						""
+					)}
+				</div>
+			</div>
+			{enriched ? (
+				<div className="flex flex-row items-end justify-center gap-5">
+					<Link href="/profile/ratings">
+						<div className="flex flex-col items-center justify-center border-e-2 border-e-white-200 pe-5">
+							<div className="text-xl font-bold">5</div>
+							<div className="text-xl text-gray-400">Ratings</div>
+						</div>
+					</Link>
+					<Link href="/profile/saved">
+						<div className="flex flex-col items-center justify-center">
+							<div className="text-xl font-bold">5</div>
+							<div className="text-xl text-gray-400">Saved</div>
+						</div>
+					</Link>
+				</div>
+			) : (
+				""
+			)}
+		</div>
+	);
 }
 
-export default ProfileDetails
+export default ProfileDetails;
