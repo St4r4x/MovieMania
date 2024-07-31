140888136e571f3c696d732296c5d081d16ec312
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-07-31 11:56:22 +0200

    add carousel into profile page

--- frontend/moviemania/app/profile/page.tsx
+++ frontend/moviemania/app/profile/page.tsx
@@ -10,6 +10,16 @@ const user = {
 	prenom: "John",
 };
 
+const medias = [
+	{ id: 1, poster_path: "/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg", title: "Test", rating: 5 },
+	{ id: 2, poster_path: "/nP6RliHjxsz4irTKsxe8FRhKZYl.jpg", title: "Test", rating: 4 },
+	{ id: 3, poster_path: "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg", title: "Test", rating: 1 },
+	{ id: 4, poster_path: "/wWba3TaojhK7NdycRhoQpsG0FaH.jpg", title: "Test", rating: 3 },
+	{ id: 5, poster_path: "/yrpPYKijwdMHyTGIOd1iK1h0Xno.jpg", title: "Test", rating: 5 },
+	{ id: 6, poster_path: "/p6AbOJvMQhBmffd0PIv0u8ghWeY.jpg", title: "Test", rating: 2 },
+	{ id: 7, poster_path: "/3E53WEZJqP6aM84D8CckXx4pIHw.jpg", title: "Test", rating: 3 },
+];
+
 function Profile() {
 	return (
 		<main className="flex flex-col items-center min-h-screen w-full gap-7">
@@ -22,6 +32,17 @@ function Profile() {
 					enriched={true}
 				/>
 			</div>
+			{/* <section className="p-8">
+				<p className="text-white text-xl inline-block border-b-2 border-white pb-2 mb-3">Récemment notés</p>
+
+				<Carousel images={images} />
+			</section>
+
+			<section className="p-8">
+				<p className="text-white text-xl inline-block border-b-2 border-white pb-2 mb-3">Récemment sauvegardés</p>
+
+				<Carousel images={images} />
+			</section> */}
 		</main>
 	);
 }
