596f6c241e3424de78caea4b94c2899b6b9c749c
Jean-Marie Geronimi <88651100+s3kiro23@users.noreply.github.com>
2024-07-31 12:32:36 +0200

    Update auth-services.tsx

--- frontend/moviemania/data/services/auth-services.tsx
+++ frontend/moviemania/data/services/auth-services.tsx
@@ -2,22 +2,25 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 interface RegisterUserProps {
-	email: string;
-	password: string;
+   email: string;
+   password: string;
 }
 
 export async function registerUserService(userData: RegisterUserProps) {
-	try {
-		const response = await axios({
-			url: `${process.env.NEXT_PUBLIC_USERS_API_URL}/api/v1/users/open`,
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			data: JSON.stringify({ ...userData }),
-		});
-		return response.data;
-	} catch (error) {
-		NextResponse.json({ error });
-	}
+   try {
+      const response = await axios({
+         url: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/open`,
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         data: JSON.stringify({ ...userData }),
+      });
+
+      console.log("registerUserService", response);
+
+      return response.data;
+   } catch (error) {
+      NextResponse.json({ error });
+   }
 }
