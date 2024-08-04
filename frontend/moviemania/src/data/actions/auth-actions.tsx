b408dd3023cca2e8031909af5470d985dfeebfde
Jm GERONIMI <spawn2x@protonmail.com>
2024-08-04 18:34:37 +0200

    add password regex in front

--- frontend/moviemania/src/data/actions/auth-actions.tsx
+++ frontend/moviemania/src/data/actions/auth-actions.tsx
@@ -1,115 +1,137 @@
 "use server";
+
 import { z } from "zod";
 import { loginUserService, registerUserService } from "@/src/data/services/auth-services";
 
 const schemaRegister = z.object({
-   email: z.string().email({
-      message: "Please enter a valid email address",
-   }),
-   password: z.string().min(6).max(100, {
-      message: "Password must be between 6 and 100 characters",
-   }),
+	email: z.string().email({
+		message: "Please enter a valid email address",
+	}),
+	// password: z
+	// 	.string()
+	// 	.min(8, {
+	// 		message: "Le mot de passe doit comporter au moins 8 caractères",
+	// 	})
+	// 	.max(100, {
+	// 		message: "Le mot de passe doit comporter moins de 100 caractères",
+	// 	})
+	// 	.refine((password) => /[A-Z]/.test(password), {
+	// 		message: "Le mot de passe doit contenir au moins une lettre majuscule",
+	// 	})
+	// 	.refine((password) => /[a-z]/.test(password), {
+	// 		message: "Le mot de passe doit contenir au moins une lettre minuscule",
+	// 	})
+	// 	.refine((password) => /[0-9]/.test(password), {
+	// 		message: "Le mot de passe doit contenir au moins un chiffre",
+	// 	})
+	// 	.refine((password) => /[^a-zA-Z0-9]/.test(password), {
+	// 		message: "Le mot de passe doit contenir au moins un caractère spécial",
+	// 	}),
+	password: z.string().min(8, {
+		message: "Le mot de passe doit comporter au moins 8 caractères",
+	}),
 });
 
 export async function registerUserAction(prevState: any, formData: FormData) {
-   const validatedFields = schemaRegister.safeParse({
-      email: formData.get("email"),
-      password: formData.get("password"),
-   });
+	const validatedFields = schemaRegister.safeParse({
+		email: formData.get("email"),
+		password: formData.get("password"),
+	});
 
-   if (!validatedFields.success) {
-      return {
-         ...prevState,
-         zodErrors: validatedFields.error.flatten().fieldErrors,
-         message: "Missing Fields. Failed to Register.",
-      };
-   }
+	if (!validatedFields.success) {
+		return {
+			...prevState,
+			zodErrors: validatedFields.error.flatten().fieldErrors,
+			message: "Des champs sont manquants. Echec de l'enregistrement.",
+		};
+	}
 
-   const responseData = await registerUserService(validatedFields.data);
+	const responseData = await registerUserService(validatedFields.data);
 
-   if (!responseData) {
-      return {
-         ...prevState,
-         zodErrors: null,
-         message: "Ops! Something went wrong. Please try again.",
-      };
-   }
+	if (!responseData) {
+		return {
+			...prevState,
+			zodErrors: null,
+			message: "Oups ! Quelque chose a mal tourné. Veuillez réessayer.",
+		};
+	}
 
-   if (responseData.error) {
-      return {
-         ...prevState,
-         zodErrors: null,
-         message: "Failed to Register.",
-      };
-   }
+	if (responseData.error) {
+		return {
+			...prevState,
+			zodErrors: null,
+			message: "Echec de l'enregistrement.",
+		};
+	}
 
-   const loginData = {
-      username: validatedFields.data.email,
-      password: validatedFields.data.password,
-   }
-   const loginResponse = await loginUserService(loginData);
+	// const loginData = {
+	// 	username: validatedFields.data.email,
+	// 	password: validatedFields.data.password,
+	// };
+	// const loginResponse = await loginUserService(loginData);
 
-   if (!loginResponse || loginResponse.error) {
-      return {
-         ...prevState,
-         zodErrors: null,
-         message: "Inscription réussie, mais la connexion a échoué.",
-      };
-   }
+	// if (!loginResponse || loginResponse.error) {
+	// 	return {
+	// 		...prevState,
+	// 		zodErrors: null,
+	// 		message: "Inscription réussie, mais la connexion a échoué.",
+	// 	};
+	// }
+   
 
-   return {
-      ...prevState,
-      zodErrors: null,
-      message: "Inscription réussie!",
-   };
+	return {
+		...prevState,
+		zodErrors: null,
+		message: "Inscription réussie!",
+	};
 }
 
 const schemaLogin = z.object({
-   email: z.string().email({
-      message: "Please enter a valid email address",
-   }),
-   password: z.string().min(6).max(100, {
-      message: "Password must be between 6 and 100 characters",
-   }),
+	email: z.string().email({
+		message: "Veuillez entrer une adresse email valide",
+	}),
+	password: z.string().min(6).max(100, {
+		message: "Le mot de passe doit comporter au moins 8 caractères",
+	}),
 });
 
 export async function loginUserAction(prevState: any, formData: FormData) {
-   const validatedFields = schemaLogin.safeParse({
-      email: formData.get("email"),
-      password: formData.get("password"),
-   });
+	const validatedFields = schemaLogin.safeParse({
+		email: formData.get("email"),
+		password: formData.get("password"),
+	});
 
-   if (!validatedFields.success) {
-      return {
-         ...prevState,
-         zodErrors: validatedFields.error.flatten().fieldErrors,
-         message: "Missing Fields. Failed to Login.",
-      };
-   }
+	if (!validatedFields.success) {
+		return {
+			...prevState,
+			zodErrors: validatedFields.error.flatten().fieldErrors,
+			message: "Champs manquants. Echec de la connexion.",
+		};
+	}
 
-   const responseData = await loginUserService(validatedFields.data);
+	const responseData = await loginUserService(validatedFields.data);
 
-   console.log("loginUserAction responseData", responseData);
+	console.log("loginUserAction responseData", responseData);
 
-   if (!responseData) {
-      return {
-         ...prevState,
-         zodErrors: null,
-         message: "Ops! Something went wrong. Please try again.",
-      };
-   }
+	if (!responseData) {
+		return {
+			...prevState,
+			zodErrors: null,
+			message: "Oups ! Quelque chose a mal tourné. Veuillez réessayer.",
+		};
+	}
 
-   if (responseData.error) {
-      return {
-         ...prevState,
-         zodErrors: null,
-         message: "Failed to Login.",
-      };
-   }
+	if (responseData.error) {
+		return {
+			...prevState,
+			zodErrors: null,
+			message: "Echec de la connexion.",
+		};
+	}
 
-   return {
-      ...prevState,
-      zodErrors: null,
-      message: "Login successful!",
-   };
+	return {
+		...prevState,
+		zodErrors: null,
+		message: "Connexion réussie!",
+	};
 }
