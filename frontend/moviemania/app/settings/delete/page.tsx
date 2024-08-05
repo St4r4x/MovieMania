8c069d3aaf02f27b206ee463c9a4c900d33834a9
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-08-05 16:53:50 +0200

    delete justify center for all settings pages

--- frontend/moviemania/app/settings/delete/page.tsx
+++ frontend/moviemania/app/settings/delete/page.tsx
@@ -9,7 +9,7 @@ const SettingsDelete = async () => {
 	const userData = await getUserProfile(session);
 
 	return (
-		<main className="flex flex-col items-center justify-center min-h-screen p-5">
+		<main className="flex flex-col items-center min-h-screen p-5">
 			<div className="bg-gray-700 rounded-md p-5 flex flex-col gap-10 text-center w-full sm:w-500">
 				<h1 className="text-2xl text-white font-bold">Suppression du compte</h1>
 				<DeleteForm user={userData} />
