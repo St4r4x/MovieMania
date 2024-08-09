11e6a19c4750d8100fc4ceb9b8adcfe58c37ad02
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-08-09 10:04:53 +0200

    add pytestcache in dockerignore + change cors order

--- users_api/app/main.py
+++ users_api/app/main.py
@@ -20,12 +20,12 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
         allow_origins=[
             str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
         ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
