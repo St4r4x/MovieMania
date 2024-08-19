32d4b7a3513fa9a92dd53611ce989eadefd5fb2f
Jm GERONIMI <spawn2x@protonmail.com>
2024-08-19 20:23:31 +0200

    add moviegenre service

--- frontend/moviemania/src/components/forms/PreferencesForm.tsx
+++ frontend/moviemania/src/components/forms/PreferencesForm.tsx
@@ -1,29 +1,16 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import Image from "next/image";
+
 import { registerUserService } from "@/src/data/services/auth-services";
 import { Button } from "@/src/components/ui/button";
 import { useRouter } from "next/navigation";
+import SelectedGenres from "@/src/components/selectedGenres/SelectedGenres";
 
-const genresTable = {
-	action: { id: 1, image: "action.png" },
-	animation: { id: 2, image: "animation.webp" },
-	aventure: { id: 3, image: "aventure.jpeg" },
-	comédie: { id: 4, image: "comédie.jpg" },
-	documentaire: { id: 5, image: "documentaire.jpg" },
-	drame: { id: 6, image: "drame.jpg" },
-	fantastique: { id: 7, image: "fantastique.jpg" },
-	guerre: { id: 8, image: "guerre.jpg" },
-	histoire: { id: 9, image: "histoire.jpg" },
-	horreur: { id: 10, image: "horreur.jpg" },
-	musique: { id: 11, image: "musique.jpeg" },
-	romance: { id: 12, image: "romance.webp" },
-	scifi: { id: 13, image: "scifi.jpeg" },
-	thriller: { id: 14, image: "thriller.jpeg" },
-	western: { id: 15, image: "western.jpg" },
-	crime: { id: 16, image: "crime.jpg" },
-};
+interface FormData {
+	get: (key: string) => string | null;
+}
 
 interface PreferencesFormProps {
 	onBackClick: () => void;
@@ -62,33 +49,11 @@ export default function PreferencesForm({ onBackClick, formData }: PreferencesFo
 		}
 	};
 
-	const capitalizeFirstLetter = (string: string) => {
-		return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-	};
-
 	return (
 		<form onSubmit={handleFormSubmit}>
 			<div className="flex flex-col md:flex-row gap-16 items-center justify-center">
 				<div className="grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 border-t-2 pt-10 md:pt-14">
-					{Object.entries(genresTable).map(([genre, { id, image }]) => (
-						<div
-							key={id}
-							className="rounded-lg bg-no-repeat bg-cover bg-center cursor-pointer transition-transform transform hover:scale-105 flex justify-center items-center"
-							style={{ backgroundImage: `url(/${image})` }}
-						>
-							<label className="cursor-pointer flex flex-col items-center">
-								<input
-									type="checkbox"
-									className="checkbox absolute hidden peer"
-									name="genres"
-									value={id}
-								/>
-								<span className="p-3 sm:p-6 md:p-14 font-bold bg-opacity-50 bg-black rounded-lg md:text-2xl text-center text-white w-40 sm:w-44 lg:w-72 border-transparent border-4 flex-wrap peer-checked:border-primary peer-checked:text-primary">
-									{capitalizeFirstLetter(genre)}
-								</span>
-							</label>
-						</div>
-					))}
+					<SelectedGenres />
 				</div>
 				<Button
 					className="w-full md:hidden"
--- frontend/moviemania/src/data/services/movie-services.tsx
+++ frontend/moviemania/src/data/services/movie-services.tsx
@@ -1,23 +1,17 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMxOTU5MzYsInN1YiI6IjEifQ.dEzxsfR8tUuyoV8J6_lxoe163xDv3zhJ1FKgrjzgA3s";
-
-export const getAllMovieGenres = async (genres: string[]) => {
+export const getAllMovieGenres = async () => {
 	try {
 		const response = await axios({
 			url: `${process.env.NEXT_PUBLIC_RECOS_API_URL}/genres/`,
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json",
-				//Authorization: `Bearer ${session?.access_token}`,
-				Authorization: `Bearer ${token}`,
 			},
 		});
 		if (response.status === 200) {
-			//router.push("/home");
-            console.log(response.data);
-            return response.data;
+			return response.data;
 		}
 	} catch (error) {
 		NextResponse.json({ error });
