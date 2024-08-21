1c1c9917483284cf6285ce0cd360043c75a8a22e
Jm GERONIMI <spawn2x@protonmail.com>
2024-08-21 21:09:37 +0200

    delete unused route.tsx nextauth + import

--- frontend/moviemania/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/moviemania/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "axios";
 
